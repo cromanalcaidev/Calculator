27ef69c8227c80a6f9e10166ba9510715401b5d3
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-23 06:53:02 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Carlos Román Alcaide on 23/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
